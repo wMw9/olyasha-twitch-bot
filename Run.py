5d9e2b9537ad659864fd2945570121e447f6c8ad
wmw <wmwofficial@gmail.com>
2018-04-03 15:55:59 +0300

    0

--- Run.py
+++ Run.py
@@ -36,7 +36,7 @@ while True:
 				i = isSub(line)
 				if ("subscriber=1" in i) or ("@badges=moderator" in i) or ("@badges=broadcaster" in i):
 					#print ('Да, он саб!')
-					sendWhisper(s, user, 'Привет, '+ user +'. Добро пожаловать в тайный чатик утиной армию, НИКОМУ не показывай ссылку!: ' + INVITE)
+					sendWhisper(s, user, 'ehfПривет, '+ user +'. Добро пожаловать в тайный чатик утиной армию, НИКОМУ не показывай ссылку!: ' + INVITE)
 					break
 				#print ('Не саб!')
 				sendWhisper(s, user, 'Прости, ' + user + '... но ты не подписчик Оляши. Саб-чат в телеграме доступен только для солдатов утиной армии')
